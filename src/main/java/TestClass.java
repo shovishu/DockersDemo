4ecdaf3e33cf92de00c19654bd4233ad472344e3
shovishu <shovishu@gmail.com>
2025-05-29 17:52:11 +0530

    Selenium Grid Tutorials

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,53 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class TestClass {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void launchBrowser() throws MalformedURLException {
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("browserName", "chrome");
+
+        driver = new RemoteWebDriver(new URL("http://192.168.14.246:4444/wd/hub"), options);
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+
+    }
+
+    @Test
+    public void getTitle1(){
+        driver.get("https://www.google.com");
+        String title = driver.getTitle();
+        System.out.println(title);
+    }
+
+    @Test
+    public void getTitle2(){
+        driver.get("https://www.ottplay.com");
+        String title = driver.getTitle();
+        System.out.println(title);
+    }
+
+    @Test
+    public void getTitle3(){
+        driver.get("https://www.netflix.com");
+        String title = driver.getTitle();
+        System.out.println(title);
+    }
+
+    @AfterMethod
+    public void quitBrowser(){
+        driver.quit();
+    }
+}
